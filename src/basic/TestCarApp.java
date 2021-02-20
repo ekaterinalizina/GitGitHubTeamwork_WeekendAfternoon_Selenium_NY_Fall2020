37002444c98cda46f8421fa8a081a57395845d41
Arthur <aadjamoglian@gmail.com>
2021-02-20 13:09:35 -0800

    Test cases

--- src/basic/TestCarApp.java
+++ src/basic/TestCarApp.java
@@ -1,4 +1,23 @@
 package basic;
 
+import cars.Bmw;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestCarApp {
+
+    @Test
+    public void testStartCar() {
+        String expectedResult = "Car has started";
+        String actualResult = Bmw.StartCar();
+        Assert.assertEquals("Test Fail", expectedResult, actualResult);
+    }
+
+    @Test
+    public void testAccelerateCar() {
+        String expectedResult = "Car accelerates";
+        String actualResult = Bmw.AccelerateCar();
+        Assert.assertEquals("Test Fail", expectedResult, actualResult);
+    }
+
 }
--- src/cars/Bmw.java
+++ src/cars/Bmw.java
@@ -2,10 +2,40 @@ package cars;
 
 public class Bmw {
 
-
+    public static
 
     // Create 4 variables
     // 2 or 3 Constructors
     // 2 or 3 Methods
+
+    String brand = "BMW";
+    int cost = 70000;
+    String engine = "V6";
+    String modelName = "M6";
+
+    public Bmw(int cost) {
+        this.cost = cost;
+    }
+
+    public Bmw(int cost, String engine) {
+        this.cost = cost;
+        this.engine = engine;
+    }
+
+    public Bmw(int cost, String engine, String modelName) {
+        this.cost = cost;
+        this.engine = engine;
+        this.modelName = modelName;
+    }
+
+    public static String StartCar() {
+        String carStarted = "Car has started";
+        return carStarted;
+    }
+
+    public static String AccelerateCar() {
+        String carAccelerates = "Car accelerates";
+        return carAccelerates;
+    }
 
 }
