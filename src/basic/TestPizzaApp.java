578484c99eb873cf30eb2c5fd83bc8cd304df8f8
Dztester2013 <Dztester2013@gmail.com>
2021-02-20 14:11:01 -0700

    Alili parts test updated

--- src/basic/TestPizzaApp.java
+++ src/basic/TestPizzaApp.java
@@ -1,5 +1,28 @@
 package basic;
 
-public class TestPizzaApp {
+import Pizza.AppTest;
 
+public class TestPizzaApp {
+    public static void main(String[] args) {
+        String model="Hp";
+        String lmodel="Acer";
+        String pmodel="Acer";
+
+        int  lver= 10;
+
+        String vendor="IBM";
+        String ver="2.0";
+        int var =2021;
+        int val=15;
+        AppTest test=new AppTest();
+        AppTest test1=new AppTest(var);
+        AppTest test2=new AppTest(var,val);
+        AppTest test3=new AppTest(var,val,model,vendor);
+        AppTest.compter(model);
+
+        AppTest.laptop(lmodel,lver);
+        AppTest.phone(pmodel);
+
+
+    }
 }
